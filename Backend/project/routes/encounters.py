e081a51777a65e0b0c61ae82151a17109c00e3fe
P97Danny <P97Danny@users.noreply.github.com>
2025-12-05 20:32:09 +0100

    Added encounter routes

---
+++ Backend/project/routes/encounters.py
@@ -0,0 +1,67 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, status
+
+from db.models.encounter import EncounterResponse, PaginatedEncounterResponse
+from db.models.enums import EncounterStatusEnum, EncounterTypeEnum
+from project.services.encounter_service import (
+    create_encounter_service,
+    delete_encounter_service,
+    get_encounter_by_id_service,
+    get_encounters_service,
+    update_encounter_service,
+)
+
+router = APIRouter(
+    prefix="/encounter",
+    responses={404: {"description": "Encounter not found"}},
+    tags=["Encounters"],
+)
+
+
+@router.get("/", status_code=status.HTTP_200_OK)
+async def get_encounters(
+    page: int | None = None,
+    limit: int | None = None,
+    encounter_id: int | None = None,
+    encounter_status: EncounterStatusEnum | None = None,
+    encounter_type: EncounterTypeEnum | None = None,
+) -> PaginatedEncounterResponse:
+    encounter_result = await get_encounters_service(
+        page=page,
+        limit=limit,
+        encounter_id=encounter_id,
+        encounter_status=encounter_status,
+        encounter_type=encounter_type,
+    )
+    return encounter_result
+
+
+@router.get("/{encounter_id}", status_code=status.HTTP_200_OK)
+async def get_encounter(encounter_id: int) -> EncounterResponse:
+    encounter = await get_encounter_by_id_service(encounter_id=encounter_id)
+    return encounter
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_encounter(
+    form_data: Annotated[EncounterResponse, Depends()],
+) -> EncounterResponse:
+    encounter = await create_encounter_service(form_data=form_data)
+    return encounter
+
+
+@router.put("/{encounter_id}", status_code=status.HTTP_200_OK)
+async def update_encounter(
+    encounter_id: int,
+    form_data: Annotated[EncounterResponse, Depends()],
+) -> EncounterResponse:
+    encounter = await update_encounter_service(
+        encounter_id=encounter_id, form_data=form_data
+    )
+    return encounter
+
+
+@router.delete("/{encounter_id}", status_code=status.HTTP_200_OK)
+async def delete_encounter(encounter_id: int) -> None:
+    await delete_encounter_service(encounter_id=encounter_id)
---
+++ Backend/project/services/encounter_service.py
@@ -0,0 +1,152 @@
+from typing import Annotated
+
+import httpx
+from fastapi import Depends, HTTPException, status
+
+from db.models.encounter import EncounterResponse, PaginatedEncounterResponse
+from db.models.enums import EncounterStatusEnum, EncounterTypeEnum
+from project.globals import EPD_URL
+
+url_prefix = f"{EPD_URL}/encounters"
+
+
+async def get_encounters_service(
+    page: int | None = None,
+    limit: int | None = None,
+    encounter_id: int | None = None,
+    encounter_status: EncounterStatusEnum | None = None,
+    encounter_type: EncounterTypeEnum | None = None,
+) -> PaginatedEncounterResponse:
+    """
+    Get all encounters
+    """
+
+    epd_url = url_prefix
+    params = {
+        "page": page,
+        "limit": limit,
+        "encounterId": encounter_id,
+        "status": encounter_status,
+        "type": encounter_type,
+    }
+    params = {k: v for k, v in params.items() if v is not None}
+
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(epd_url, params=params)
+            response.raise_for_status()
+    except httpx.RequestError as exc:
+        raise HTTPException(
+            status_code=status.HTTP_502_BAD_GATEWAY, detail="EPD niet bereikbaar"
+        ) from exc
+    except httpx.HTTPStatusError as exc:
+        raise HTTPException(
+            status_code=exc.response.status_code,
+            detail=exc.response.text,
+        ) from exc
+
+    data = response.json()
+    return PaginatedEncounterResponse.model_validate(data)
+
+
+async def get_encounter_by_id_service(encounter_id: int) -> EncounterResponse:
+    """
+    Get a encounter by uuid
+    """
+    epd_url = url_prefix
+    params = {"id": encounter_id}
+
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(epd_url, params=params)
+            response.raise_for_status()
+    except httpx.RequestError as exc:
+        raise HTTPException(
+            status_code=status.HTTP_502_BAD_GATEWAY, detail="EPD niet bereikbaar"
+        ) from exc
+    except httpx.HTTPStatusError as exc:
+        raise HTTPException(
+            status_code=exc.response.status_code,
+            detail=exc.response.text,
+        ) from exc
+
+    data = response.json()
+    return EncounterResponse.model_validate(data)
+
+
+async def create_encounter_service(
+    form_data: Annotated[EncounterResponse, Depends()],
+) -> EncounterResponse:
+    """
+    Create a new encounter
+    """
+
+    epd_url = url_prefix
+    payload = form_data.model_dump(by_alias=True)
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.post(epd_url, json=payload)
+            response.raise_for_status()
+    except httpx.RequestError as exc:
+        raise HTTPException(
+            status_code=status.HTTP_502_BAD_GATEWAY, detail="EPD niet bereikbaar"
+        ) from exc
+    except httpx.HTTPStatusError as exc:
+        raise HTTPException(
+            status_code=exc.response.status_code,
+            detail=exc.response.text,
+        ) from exc
+
+    data = response.json()
+    return EncounterResponse.model_validate(data)
+
+
+async def update_encounter_service(
+    encounter_id: int, form_data: Annotated[EncounterResponse, Depends()]
+) -> EncounterResponse:
+    """
+    Update info on an encounter
+    """
+
+    epd_url = url_prefix
+    params = {"id": encounter_id}
+    payload = form_data.model_dump(by_alias=True)
+
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.put(epd_url, params=params, json=payload)
+            response.raise_for_status()
+    except httpx.RequestError as exc:
+        raise HTTPException(
+            status_code=status.HTTP_502_BAD_GATEWAY, detail="EPD niet bereikbaar"
+        ) from exc
+    except httpx.HTTPStatusError as exc:
+        raise HTTPException(
+            status_code=exc.response.status_code,
+            detail=exc.response.text,
+        ) from exc
+
+    data = response.json()
+    return EncounterResponse.model_validate(data)
+
+
+async def delete_encounter_service(encounter_id: int) -> None:
+    """
+    Delete an encounter
+    """
+    epd_url = url_prefix
+    params = {"id": encounter_id}
+
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.delete(epd_url, params=params)
+            response.raise_for_status()
+    except httpx.RequestError as exc:
+        raise HTTPException(
+            status_code=status.HTTP_502_BAD_GATEWAY, detail="EPD niet bereikbaar"
+        ) from exc
+    except httpx.HTTPStatusError as exc:
+        raise HTTPException(
+            status_code=exc.response.status_code,
+            detail=exc.response.text,
+        ) from exc
