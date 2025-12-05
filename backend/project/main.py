9c4ab6ec51a858cd40bc414086842149eed62e51
P97Danny <P97Danny@users.noreply.github.com>
2025-12-05 16:30:52 +0100

    Updated main with new routes

--- backend/project/main.py
+++ backend/project/main.py
@@ -1,24 +1,33 @@
 from fastapi import FastAPI
 
-from project.gobals import CLIENT_URL
+from project.globals import CLIENT_URL
 from project.middleware.cors import setup_cors_middleware
+from project.routes import auth, patients
 
 
 def add_middleware(app: FastAPI) -> None:
     """apply middleware handlers"""
     # Provide allowed_origins to the CORS setup. Adjust as needed for your environment.
-    setup_cors_middleware(app, allowed_origins=[CLIENT_URL])
+    if CLIENT_URL is not None:
+        setup_cors_middleware(app, allowed_origins=[CLIENT_URL])
+
 
 def build_app() -> FastAPI:
     """
     Build and configure the FastAPI app with optional settings override.
     """
     app = FastAPI(
-    root_path="/api",
+        root_path="/api",
     )
+    add_middleware(app)
     return app
 
+
 app: FastAPI = build_app()
+
+app.include_router(auth.router)
+app.include_router(patients.router)
+
 
 @app.get("/", tags=["root"])
 async def root() -> dict[str, str]:
