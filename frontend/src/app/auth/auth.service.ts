a11696c251a4e6d79bd1b550c48f6c9addb876a5
NoNamer777 <oscar.wellner@gmail.com>
2025-12-09 18:58:16 +0100

    Add simple AuthService to send login requests

---
+++ frontend/src/app/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { RequestService } from '../shared';
+import { LoginData } from './models';
+
+export class AuthService {
+    private readonly requestService = new RequestService();
+    
+    private readonly baseUrl = 'http://localhost:8000/auth';
+
+    public async login(data: LoginData) {
+        const response = await this.requestService.post(`${this.baseUrl}/login`, data);
+
+        console.log({ response });
+    }
+}
