d6e5d9c287664cadb0e9811569985243a791f0e8
NoNamer777 <oscar.wellner@gmail.com>
2025-12-09 19:01:59 +0100

    Prettier

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -3,11 +3,14 @@ import { LoginData } from './models';
 
 export class AuthService {
     private readonly requestService = new RequestService();
-    
+
     private readonly baseUrl = 'http://localhost:8000/auth';
 
     public async login(data: LoginData) {
-        const response = await this.requestService.post(`${this.baseUrl}/login`, data);
+        const response = await this.requestService.post(
+            `${this.baseUrl}/login`,
+            data,
+        );
 
         console.log({ response });
     }
