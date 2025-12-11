a300b7295631dc990c827e618bf971db8599261c
NoNamer777 <oscar.wellner@gmail.com>
2025-12-11 18:57:00 +0100

    Use auth0 to log in and out

--- frontend/src/app/core/navbar/navbar.tsx
+++ frontend/src/app/core/navbar/navbar.tsx
@@ -1,7 +1,12 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import { Link } from 'react-router-dom';
+import LogInButton from '../../auth/log-in-button.tsx';
+import LogOutButton from '../../auth/log-out-button.tsx';
 import './navbar.css';
 
 export default function Navbar() {
+    const { isLoading, isAuthenticated } = useAuth0();
+
     return (
         <nav className="navbar navbar-diapedis navbar-expand-lg px-3">
             <Link className="navbar-brand text-white fw-bold" to="/home">
@@ -10,6 +15,16 @@ export default function Navbar() {
 
             <section className="collapse navbar-collapse" id="navbarMenu">
                 <ul className="navbar-nav ms-auto text-end">
+                    {!isLoading && !isAuthenticated && (
+                        <li className="nav-item">
+                            <LogInButton />
+                        </li>
+                    )}
+                    {!isLoading && isAuthenticated && (
+                        <li className="nav-item">
+                            <LogOutButton />
+                        </li>
+                    )}
                 </ul>
             </section>
         </nav>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,5 @@
+import { Auth0Provider } from '@auth0/auth0-react';
+import type { ClientAuthorizationParams } from '@auth0/auth0-spa-js/src/global.ts';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
@@ -10,11 +12,24 @@ if (!rootElement) {
     throw new Error("Root element '#root' not found in index.html");
 }
 
+const authorizationParams = {
+    redirect_uri: window.location.origin,
+} satisfies ClientAuthorizationParams;
+
 // Create React root and render
 createRoot(rootElement).render(
     <StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <Auth0Provider
+            authorizationParams={authorizationParams}
+            cacheLocation="localstorage"
+            clientId={import.meta.env['VITE_AUTH0_CLIENT_ID']}
+            domain={import.meta.env['VITE_AUTH0_DOMAIN']}
+            useDpop={true}
+            useRefreshTokens={true}
+        >
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Auth0Provider>
     </StrictMode>,
 );
