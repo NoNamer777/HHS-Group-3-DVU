069f310001e454cd505eaf327bb326b2ad5981a5
rens meijs <13081284@student.hhs.nl>
2025-12-11 18:54:08 +0100

    Push me

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './app/app.tsx';
 import '../public/style.css';
+import App from './app/app.tsx';
 
 const rootElement = document.getElementById('root');
 
