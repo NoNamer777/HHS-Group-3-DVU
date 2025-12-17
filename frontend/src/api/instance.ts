f28a8797668d93df7fc78a94fed51c2df60d0309
rens meijs <13081284@student.hhs.nl>
2025-12-18 00:39:51 +0100

    Fixed instance

--- frontend/src/api/instance.ts
+++ frontend/src/api/instance.ts
@@ -1,30 +1,30 @@
-import axios from "axios";
-import type { AxiosRequestConfig, AxiosResponse } from "axios";
+import type { AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios from 'axios';
 
 const instance = <T>(config: AxiosRequestConfig): Promise<AxiosResponse<T>> => {
-  const instance = axios.create({
-    baseURL: "http://localhost:8000/api",
-  });
+    const instance = axios.create({
+        baseURL: 'http://localhost:8000/api',
+    });
 
-  instance.interceptors.request.use(
-    (config) => {
-      const token = localStorage.getItem("je env"); // Of hoe je ook de token ophaalt
+    instance.interceptors.request.use(
+        (config) => {
+            const token = localStorage.getItem('je env'); // Of hoe je ook de token ophaalt
 
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
-    },
-  );
+            if (token) {
+                config.headers.Authorization = `Bearer ${token}`;
+            }
+            return config;
+        },
+        (error) => {
+            return Promise.reject(error);
+        },
+    );
 
-  return instance.request<T>(config);
+    return instance.request<T>(config);
 };
 
 export const customInstance = <T>(config: AxiosRequestConfig): Promise<T> => {
-  const promise = instance({ ...config }).then((res) => res?.data as T);
+    const promise = instance({ ...config }).then((res) => res?.data as T);
 
-  return promise;
-}
+    return promise;
+};
