6c0cd9675440986be6710612806b8962f709cfc2
NoNamer777 <oscar.wellner@gmail.com>
2025-12-19 18:44:46 +0100

    Show patient info

--- frontend/src/app/patients/patient-info.page.tsx
+++ frontend/src/app/patients/patient-info.page.tsx
@@ -1,15 +1,69 @@
-import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { faArrowLeft, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useNavigate, useParams } from 'react-router-dom';
 import './patient-info.page.css';
 import { useGetPatientByIdQuery } from './patients.api.ts';
 
 export default function PatientInfoPage() {
     const { patientId } = useParams();
-    const { data: patient } = useGetPatientByIdQuery(patientId);
+    const navigate = useNavigate();
 
-    useEffect(() => {
-        console.log(patient);
-    }, [patient]);
+    const { data: patient, isLoading } = useGetPatientByIdQuery(patientId);
 
-    return <></>;
+    async function onNavigateBack() {
+        await navigate('/dashboard');
+    }
+
+    return (
+        <article className="container-fluid">
+            <button
+                type="button"
+                className="btn btn-light d-flex align-items-center gap-3 mb-5"
+                onClick={onNavigateBack}
+            >
+                <FontAwesomeIcon icon={faArrowLeft} />
+                <span className="h5 m-0">Terug naar het dashboard</span>
+            </button>
+            <div className="card shadow-sm w-100 rounded-4">
+                <div className="card-body">
+                    {isLoading && (
+                        <div className="d-flex justify-content-center align-items-center py-6">
+                            <span className="spinner-border"></span>
+                        </div>
+                    )}
+                    {!isLoading && (
+                        <div className="d-flex align-items-center gap-3 py-3 px-2">
+                            <FontAwesomeIcon icon={faUserCircle} size="6x" />
+                            <div>
+                                <p className="card-title fw-bold fs-5 mb-1">
+                                    {patient.name}
+                                </p>
+                                <p className="card-text text-muted fw-semibold d-flex gap-2 mb-0">
+                                    <span>
+                                        {new Date(
+                                            patient.dateOfBirth,
+                                        ).toLocaleDateString(['nl'])}
+                                    </span>
+                                    <span>•</span>
+                                    <span>{patient.gender}</span>
+                                </p>
+                                <p className="card-text text-muted fw-semibold d-flex gap-2 mb-0">
+                                    <span>Aandoening:</span>
+                                    <span>{patient.condition}</span>
+                                </p>
+                                <p className="card-text text-muted fw-semibold d-flex gap-2">
+                                    <span>Laatst bijgewerkt:</span>
+                                    <span>
+                                        {new Date(
+                                            patient.lastUpdated,
+                                        ).toLocaleString(['nl'])}
+                                    </span>
+                                </p>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </article>
+    );
 }
