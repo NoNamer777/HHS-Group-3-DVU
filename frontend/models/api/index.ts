ddbe6efd3990b30120c9091b190c426821349701
NoNamer777 <oscar.wellner@gmail.com>
2025-12-19 19:29:55 +0100

    Reorganize patient api models

--- frontend/models/api/index.ts
+++ frontend/models/api/index.ts
@@ -1,5 +1,1 @@
-export * from './diabetes-types.ts';
-export * from './gender.ts';
-export * from './patient-status.ts';
-export * from './patient.builder.ts';
-export * from './patient.model.ts';
+export * from './patient';
