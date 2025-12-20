3c286fb345aac8e87d3ba672b47adbcfd15c57f6
NoNamer777 <oscar.wellner@gmail.com>
2025-12-20 10:28:03 +0100

    Add initial MockUsersDB

--- frontend/test/db/db.ts
+++ frontend/test/db/db.ts
@@ -1,6 +1,9 @@
 import { mockDoctorsDB, mockPatientsDB } from './api';
+import { mockUsersDB } from './auth';
 
 export function resetMockDatabases() {
+    mockUsersDB.reset();
+
     mockPatientsDB.reset();
     mockDoctorsDB.reset();
 }
