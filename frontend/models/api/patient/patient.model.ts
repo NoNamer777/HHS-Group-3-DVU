c033aa19ecf952c58829fb029eda1784c343d95f
NoNamer777 <oscar.wellner@gmail.com>
2025-12-20 10:24:22 +0100

    Let Patient extend User

--- frontend/models/api/patient/patient.model.ts
+++ frontend/models/api/patient/patient.model.ts
@@ -1,14 +1,21 @@
+import type { User } from '../../auth';
 import { type Gender } from './gender.ts';
 import { type PatientStatus } from './patient-status.ts';
 
-export interface Patient {
-    id: string;
-    name: string;
-    dateOfBirth: number;
+export interface Patient extends User {
     gender: Gender;
     condition: string;
     status: PatientStatus;
-    lastUpdated: number;
+
+    /**
+     * Timestamp in ms since the epoch.
+     */
+    dateOfBirth: number; // Date;
+
+    /**
+     * Timestamp in ms since the epoch.
+     */
+    lastUpdated: number; // Date;
 }
 
 export type CreatePatientData = Omit<Patient, 'id' | 'lastUpdated'>;
