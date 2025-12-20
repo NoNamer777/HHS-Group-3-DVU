8cc7b1e19b2f6e42bd1eec05294ecb3f63506596
NoNamer777 <oscar.wellner@gmail.com>
2025-12-20 10:33:12 +0100

    Expand initial mock doctor request handlers

--- frontend/test/mocks/backend/handlers/api/doctor.handlers.ts
+++ frontend/test/mocks/backend/handlers/api/doctor.handlers.ts
@@ -1,3 +1,13 @@
-import { RequestHandler } from 'msw';
+import { BASE_URL } from '@/models';
+import { delay, http, HttpResponse, RequestHandler } from 'msw';
+import { mockDoctorsDB } from '../../../db';
 
-export const doctorHandlers: RequestHandler[] = [];
+const endPoint = `${BASE_URL}/doctors`;
+
+export const doctorHandlers: RequestHandler[] = [
+    http.get(endPoint, async () => {
+        await delay();
+
+        return HttpResponse.json(mockDoctorsDB.getAll());
+    }),
+];
