52fb67dafc373c7f3b96e4e41a29f98249223c89
NoNamer777 <oscar.wellner@gmail.com>
2025-12-21 10:36:15 +0100

    Add conversations api and store

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,12 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { conversationsApi } from './conversations';
 import { patientsApi } from './patients';
 
 const store = configureStore({
     reducer: {
         [patientsApi.reducerPath]: patientsApi.reducer,
+        [conversationsApi.reducerPath]: conversationsApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(patientsApi.middleware),
+        getDefaultMiddleware().concat(
+            patientsApi.middleware,
+            conversationsApi.middleware,
+        ),
 });
 
 export default store;
