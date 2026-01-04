c44a0ab521ea4051cd21b28d7edbc1db1db207d4
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-04 09:17:26 +0100

    Implement mock end-points for getting all and creating new LabResults

--- frontend/test/mocks/backend/handlers/api/lab-results.handlers.ts
+++ frontend/test/mocks/backend/handlers/api/lab-results.handlers.ts
@@ -1,12 +1,37 @@
-import { BASE_URL } from '@/models';
+import {
+    BASE_URL,
+    type CreateLabResultData,
+    HttpStatusCodes,
+    HttpStatusNames,
+} from '@/models';
 import { delay, http, HttpResponse, type RequestHandler } from 'msw';
+import { mockLabResultsDB } from '../../../db';
 
 const endPoint = `${BASE_URL}/api/lab-results`;
 
 export const labResultsHandlers: RequestHandler[] = [
-    http.get(endPoint, async () => {
+    http.get(endPoint, async ({ request }) => {
         await delay();
 
-        return HttpResponse.json([]);
+        const url = new URL(request.url);
+
+        return HttpResponse.json(mockLabResultsDB.getAll(url.searchParams));
+    }),
+    http.post<null, CreateLabResultData>(endPoint, async ({ request }) => {
+        await delay();
+
+        const data = await request.json();
+        const created = mockLabResultsDB.create(data);
+
+        const status = HttpStatusCodes.CREATED;
+        const location = `${endPoint}/${created.id}`;
+
+        return HttpResponse.json(data, {
+            status: status,
+            statusText: HttpStatusNames[status],
+            headers: {
+                Location: location,
+            },
+        });
     }),
 ];
--- frontend/test/mocks/db/api/lab-results.ts
+++ frontend/test/mocks/db/api/lab-results.ts
@@ -1,11 +1,40 @@
-import type { LabResult } from '@/models';
+import type { CreateLabResultData, LabResult } from '@/models';
+import { nanoid } from 'nanoid';
 import type { Records } from '../../types';
 
 class MockLabResultsDB {
     private records: Records<LabResult> = {};
 
-    public getAll() {
-        return Object.values(this.records);
+    public getAll(params: URLSearchParams) {
+        let labResults = Object.values(this.records);
+
+        if (params.has('patient')) {
+            labResults = labResults.filter(
+                ({ patient }) => patient.id === params.get('patient'),
+            );
+        }
+        if (params.has('limit')) {
+            labResults = labResults.slice(
+                0,
+                Number.parseInt(params.get('limit')) - 1,
+            );
+        }
+        return labResults.sort((curr, next) => next.timestamp - curr.timestamp);
+    }
+
+    public create(data: CreateLabResultData) {
+        const labResult: LabResult = {
+            id: nanoid(),
+            patient: data.patient,
+            type: data.type,
+            value: data.value,
+            unit: data.unit,
+            maxValue: data.maxValue,
+            minValue: data.minValue,
+            timestamp: new Date().getTime(),
+        };
+        this.records[labResult.id] = labResult;
+        return labResult;
     }
 
     public reset() {
