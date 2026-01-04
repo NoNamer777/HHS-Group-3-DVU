d6a4bc466cde8cb665c88674ba67545b4600f244
Oscar Wellner <oscar.wellner@gmail.com>
2026-01-04 01:41:19 +0100

    Show recent conversations on patient dossier overview

--- frontend/src/app/patients/patient-info/sections/overview/patient-info-overview.component.tsx
+++ frontend/src/app/patients/patient-info/sections/overview/patient-info-overview.component.tsx
@@ -1,5 +1,32 @@
+import { useParams } from 'react-router-dom';
+import { useGetConversationsQuery } from '../../../../conversations';
+import ConversationCardComponent from '../../../../conversations/conversation-card.component.tsx';
 import './patient-info-overview.component.css';
 
 export default function PatientInfoOverviewComponent() {
-    return <p>PatientInfoOverviewComponent works!</p>;
+    const { patientId } = useParams();
+
+    const { data: conversations } = useGetConversationsQuery({
+        to: patientId,
+        limit: 3,
+    });
+
+    return (
+        <div className="d-flex flex-column gap-3">
+            <div className="card rounded-4 w-100 shadow-sm">
+                <div className="card-body"></div>
+            </div>
+            <div className="card rounded-4 w-100 shadow-sm">
+                <div className="card-body">
+                    <h5 className="card-title">Recente communicatie</h5>
+                    {(conversations ?? []).map((conversation) => (
+                        <ConversationCardComponent
+                            conversation={conversation}
+                            key={conversation.id}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
